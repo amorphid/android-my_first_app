6d54875e36ff684de3961b56858ed4848e6d1eab
Michael Pope <mpope.cr@gmail.com>
2015-01-05 14:54:20 -0500

    Display the message

--- app/src/main/java/com/example/pope4president/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/pope4president/myfirstapp/DisplayMessageActivity.java
@@ -5,14 +5,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the tet view as the activity layout
+        setContentView(textView);
     }
 
     @Override
